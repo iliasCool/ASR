0113cd89704cc14e4ccdd712e08b2c0cf9295476
michem32 <29971931+michem32@users.noreply.github.com>
2017-11-21 14:45:17 +0200

    Update MessageToASR.java

--- src/main/java/com/example/MessageToASR.java
+++ src/main/java/com/example/MessageToASR.java
@@ -2,7 +2,7 @@ package com.example;
 
 /**
 *
-* @author andreadisst
+* This is being used in order to read the Json message from hub and extract audio URL and timestamp
 */
 public class MessageToASR {
    
