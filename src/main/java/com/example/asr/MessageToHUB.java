b0c2d73e52b00e6650b635f091c3ecf7fc8f017f
michem32 <29971931+michem32@users.noreply.github.com>
2017-12-15 18:02:18 +0200

    Update MessageToHUB.java

--- src/main/java/com/example/asr/MessageToHUB.java
+++ src/main/java/com/example/asr/MessageToHUB.java
@@ -13,8 +13,8 @@ public class MessageToHUB {
        
    }
    
-   public MessageToHUB(String timestamp, String language, String transcrpition_id){
-       this.message = new MessageToHUBContent(URL, timestamp,language,incidentID);
+   public MessageToHUB(String timestamp, String language, String IDRef){
+       this.message = new MessageToHUBContent(timestamp,language,IDRef);
    }
    
 }
