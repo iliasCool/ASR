96e425a22b4445b618e55dbf8889f0267d0701e1
michem32 <29971931+michem32@users.noreply.github.com>
2018-01-09 16:12:03 +0200

    Update Transcriber.java

--- src/main/java/com/example/asr/Transcriber.java
+++ src/main/java/com/example/asr/Transcriber.java
@@ -29,7 +29,7 @@ public class Transcriber {
 public static void main(String[] args) {
 	
 }
-public static String transcribe(String fileURL) throws IOException {
+public static String transcribe(String fileURL, String language) throws IOException {
 	// this is used to upload a file from workspace to Raw data Store 
 	//RawDataStore.storeFile("test.wav","test.wav");
 
@@ -38,10 +38,48 @@ public static String transcribe(String fileURL) throws IOException {
    
 	// 2. Load ASR configuration 
 	Configuration configuration = new Configuration();
-	//English
-	configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
-        configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
-        configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+	
+
+    switch (language) {
+    case "en-US": //English online
+    	configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+    	configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+    	configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+        // //alternativelly 
+        	//configuration.setAcousticModelPath("file:acoustic_models/English/en-us");
+        	//configuration.setDictionaryPath("file:acoustic_models/English/cmudict-en-us.dict");
+        	//configuration.setLanguageModelPath("file:acoustic_models/English/en-us.lm.bin");
+        break;
+    case "it-IT"://Italian 
+    	configuration.setAcousticModelPath("file:acoustic_models/Italian/cmusphinx-it-5.2/model_parameters/voxforge_it_sphinx.cd_cont_2000");
+    	configuration.setDictionaryPath("file:acoustic_models/Italian/cmusphinx-it-5.2/etc/voxforge_it_sphinx.dic");
+    	configuration.setLanguageModelPath("file:acoustic_models/Italian/cmusphinx-it-5.2/etc/voxforge_it_sphinx.lm");
+        // //alternativelly 
+    		//configuration.setAcousticModelPath("file:acoustic_models/Italian/cmusphinx-it-5.2/model_parameters/voxforge_it_sphinx.cd_cont_2000");
+    		//configuration.setDictionaryPath("file:acoustic_models/Italian/voxforge_it_sphinx/etc/voxforge_it.dic");
+    		//configuration.setLanguageModelPath("file:acoustic_models/Italian/voxforge_it_sphinx/etc/voxforge_it.lm.dmp");
+        break;
+    case "es"://Spanish 16kHz model
+    	configuration.setAcousticModelPath("file:acoustic_models/Spanish/cmu_spanish/cmusphinx-es-5.2/model_parameters/voxforge_es_sphinx.cd_ptm_4000");
+    	configuration.setDictionaryPath("file:acoustic_models/Spanish/cmu_spanish/es.dict");
+    	//configuration.setDictionaryPath("file:acoustic_models/Spanish/cmu_spanish/cmusphinx-es-5.2/etc/voxforge_es_sphinx");//words missing from the dictionary
+    	configuration.setLanguageModelPath("file:acoustic_models/Spanish/cmu_spanish/es-20k.lm");
+        // //alternativelly 
+        break;
+    case "el-GR"://Greek 
+        configuration.setAcousticModelPath("file:acoustic_models/Greek/cmusphinx-el-gr-5.2/el-gr.cd_cont_5000");
+        configuration.setDictionaryPath("file:acoustic_models/Greek/cmusphinx-el-gr-5.2/el-gr.dic");
+        configuration.setLanguageModelPath("file:acoustic_models/Greek/cmusphinx-el-gr-5.2/el-gr.lm.bin");
+        //configuration.setLanguageModelPath("file:acoustic_models/Greek/cmusphinx-el-gr-5.2/el-gr.lm");
+        break;
+}
+
+	
+	
+	
+	
+	
+
        
        // configuration.setAcousticModelPath("file:acoustic_models/English/en-us");
        // configuration.setDictionaryPath("file:acoustic_models/English/cmudict-en-us.dict");
@@ -51,9 +89,10 @@ public static String transcribe(String fileURL) throws IOException {
         StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
     	InputStream stream = new FileInputStream(new File("new_audio.wav"));
 
-  	//   InputStream stream = new FileInputStream(new File("msg4.wav"));
+//  	   InputStream stream = new FileInputStream(new File("spanish16KHz2.wav"));
+	  // InputStream stream = new FileInputStream(new File("el-0128_16kHz.wav"));
     	
-
+        
     	recognizer.startRecognition(stream);
         SpeechResult result;
         String transcription=""; 
