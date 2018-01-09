d3b49c7202b7d8fdca04c78280689fb64f9cf004
michem32 <29971931+michem32@users.noreply.github.com>
2018-01-09 17:00:32 +0200

    Update Test.java

--- src/main/java/com/example/asr/Test.java
+++ src/main/java/com/example/asr/Test.java
@@ -8,11 +8,34 @@ public class Test {
 
 	public static void main(String[] args) throws IOException, KeyManagementException, NoSuchAlgorithmException {
 		// TODO Auto-generated method stub
+		String lang_id="en";   //Define language
+
 		
-		String url="http://object-store-app.eu-gb.mybluemix.net/objectStorage?file=test.wav";
+		
+		String language="en-US";
+		String url="http://object-store-app.eu-gb.mybluemix.net/objectStorage?file=test.wav"; //english msg6
+		switch (lang_id) {
+		case "en":
+			language="en-US";
+			url="http://object-store-app.eu-gb.mybluemix.net/objectStorage?file=english_rec.wav";
+			break;
+	    case "el":
+			language="el-GR";
+			url="http://object-store-app.eu-gb.mybluemix.net/objectStorage?file=greek_rec.wav";
+			break;
+	    case "es":
+			language="es";
+			url="http://object-store-app.eu-gb.mybluemix.net/objectStorage?file=spanish_rec.wav";
+			break;
+	    case "it":
+			language="it-IT";
+			url="http://object-store-app.eu-gb.mybluemix.net/objectStorage?file=italian_rec.wav";
+			break;
+		}
+		
 		String audio_timestamp="2018-01-01T12:00:00Z";
 		String transcription;
-   		String language="el-GR";
+   		
 		transcription =   Transcriber.transcribe(url,language);
 		//String transcription_id="test" ;
    		String incidentID="542845";
@@ -22,4 +45,3 @@ public class Test {
 
 	}
 }
-
